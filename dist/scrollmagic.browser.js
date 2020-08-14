5d7ffebfe77441eb9c458e0e914a28e9f3c631d2
Jan Paepke <jan@paepke.at>
2020-08-14 15:01:24 +0200

    changed output

--- dist/scrollmagic.browser.js
+++
@@ -1,1 +0,0 @@
-var ScrollMagic=function(c){"use strict";var i=function(){console.log("init")};return c.ScrollMagic=i,c}({});
--- dist/scrollmagic.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=function(){console.log("init")};exports.ScrollMagic=e;
