b95a971afaaadc9e9bbad47be51ab9b6ba26608f
Jan Paepke <jan@paepke.at>
2020-08-15 14:10:06 +0200

    finished build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 import pkg from './package.json';
 
+import path from 'path';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
+const inputFile = 'src/scrollmagic.ts';
 const devMode = process.env.ROLLUP_WATCH;
 
 const umdOutput = {
 	format: 'umd',
 	file: pkg.main,
 	name: pkg.title, // var name of browser global
+	sourcemap: true,
 };
 
 const esmOutput = {
@@ -17,8 +20,19 @@ const esmOutput = {
 	sourcemap: true,
 };
 
+const tsPluginConfig = {
+	useTsconfigDeclarationDir: true,
+	tsconfigOverride: {
+		compilerOptions: {
+			declaration: true,
+			declarationMap: true,
+			declarationDir: path.dirname(pkg.types),
+		},
+	},
+};
+
 export default {
-	input: 'src/scrollmagic.ts',
+	input: inputFile,
 	output: [umdOutput, esmOutput],
-	plugins: [typescript(), !devMode && terser()],
+	plugins: [typescript(tsPluginConfig), !devMode && terser()],
 };
