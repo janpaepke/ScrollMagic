12bdecc18a27ef5e99c4fca45507c13424b82489
Jan Paepke <jan@paepke.at>
2021-05-27 21:09:58 +0200

    Added documentation for ExecutionQueue

--- src/ExecutionQueue.ts
+++ src/ExecutionQueue.ts
@@ -1,21 +1,91 @@
 import { throttleRaf } from './util/throttleRaf';
 import { isUndefined } from './util/typeguards';
 
-type Command = () => void;
+type Callback = () => void;
 type ExecutionCondition = () => boolean;
-type CommandList<T extends string> = Record<T, QueueItem>;
+type CommandList<T extends string> = Record<T, Command>;
 
 /**
- * TODO! Update - this is very different now
- * Holds a list of commands and execute them in order.
- * If a command is added twice before executing, it will stay in the order position as before.
- * Caveats:
- * - can't override commands with existing precondition
+ * This class holds a list of callbacks allows them to be scheduled for execution on next animationFrame.
+ * Every callback will only be executed once per animationFrame, even if scheduled multiple times.
+ * The order of the queue superceeds the order of scheduling, this means that if the queue consists of callbacks a, b,
+ * a will always execute first, even if b is scheduled first.
+ *
+ * usage example:
+ * ```
+ * const queue = new ExecutionQueue({
+ * 		a: () => console.log('a');
+ * 		b: () => console.log('b');
+ * })
+ * queue.commands.b.schedule();
+ * queue.commands.a.schedule();
+ * <expected output on animationFrame>
+ * 'a'
+ * 'b'
+ * ```
+ *
+ * For details about conditional execution see Command class below.
+ *
+ * To invoke execution now (and purge scheduled), call queue.execute.
+ * To cancel scheduled execution, call queue.cancel
  */
+export class ExecutionQueue<C extends string> {
+	public readonly commands: CommandList<C>;
+	protected executeThrottled = throttleRaf(this.execute.bind(this));
 
-class QueueItem {
+	constructor(queueItems: Record<C, Callback>) {
+		this.commands = Object.entries<Callback>(queueItems).reduce(
+			(res, [name, command]) => ({
+				...res,
+				[name]: new Command(command, this.executeThrottled),
+			}),
+			{} as CommandList<C>
+		);
+	}
+
+	// executes all commands in the list in order, depending on wether or not their conditions are met
+	public execute(): void {
+		Object.values<Command>(this.commands).forEach(item => {
+			if (item.conditionsMet) {
+				item.execute();
+			}
+			item.resetConditions();
+		});
+	}
+	public cancel(): void {
+		this.executeThrottled.cancel();
+	}
+}
+
+/**
+ * Each command in the ExecutionQueue above can be scheduled for execution using command.schedule()
+ * .schedule() also accepts an optional parameter, a condition callback
+ * This is called when execution is due, to determine if the callback should still be called.
+ *
+ * usage example:
+ * ```
+ * let x = 1;
+ * const queue = new ExecutionQueue({
+ * 		a: () => {
+ * 			x = 2;
+ * 			console.log('a');
+ * 		};
+ * 		b: () => console.log('b');
+ * })
+ * // result of execution condition remains true
+ * queue.commands.b.schedule(() => x === 1);
+ * <expected output on animationFrame>
+ * 'b'
+ * // x is 1 now, but will be 2, once a has been called.
+ * queue.commands.a.schedule();
+ * queue.commands.b.schedule(() => x === 1);
+ * <expected output on animationFrame>
+ * 'a'
+ * ```
+ */
+class Command {
 	protected conditions: ExecutionCondition[] = [];
-	constructor(public readonly execute: Command, protected readonly onSchedule: () => void) {}
+	constructor(public readonly execute: Callback, protected readonly onSchedule: () => void) {}
 	public schedule(condition?: ExecutionCondition) {
 		if (isUndefined(condition)) {
 			// if no condition is provided, conditions are considered always met. Any conditions added after this won't even be run
@@ -30,33 +100,5 @@ class QueueItem {
 	}
 	public get conditionsMet() {
 		return this.conditions.some(condition => condition());
-	}
-}
-
-export class ExecutionQueue<C extends string> {
-	public readonly commands: CommandList<C>;
-	protected executeThrottled = throttleRaf(this.execute.bind(this));
-
-	constructor(queueItems: Record<C, Command>) {
-		this.commands = Object.entries<Command>(queueItems).reduce(
-			(res, [name, command]) => ({
-				...res,
-				[name]: new QueueItem(command, this.executeThrottled),
-			}),
-			{} as CommandList<C>
-		);
-	}
-
-	// executes all commands in the list in order, depending on wether or not their conditions are met
-	public execute(): void {
-		Object.values<QueueItem>(this.commands).forEach(item => {
-			if (item.conditionsMet) {
-				item.execute();
-			}
-			item.resetConditions();
-		});
-	}
-	public cancel(): void {
-		this.executeThrottled.cancel();
 	}
 }
